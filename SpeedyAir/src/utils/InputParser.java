bb1b59529ca5a7c702daf44f2c72ac9bbd74e30a
Tariq Syed <tariqsyed@Tariqs-MacBook-Air.local>
2025-01-26 19:37:48 -0500

    Add order json file to project repo

--- SpeedyAir/src/utils/InputParser.java
+++ SpeedyAir/src/utils/InputParser.java
@@ -1,4 +1,13 @@
 package utils;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputParser {
+
+    // Parse orders from the JSON File
+
+
 }
